3a86ee8bfa358ebbe555445f2a74f6b00d2e6f92
Cameron Lucas <44956222+CamSkiTheDev@users.noreply.github.com>
2019-01-26 13:32:02 -0700

    Learned how to use template literals to avoid concatinating a string using the + operator.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Variables/String-Interpolation.js
@@ -0,0 +1,6 @@
+let myName = 'Cameron Lucas';
+let myCity = 'Denver, Colorado';
+
+console.log(`My name is ${myName}. My favorite city is ${myCity}`);
+
+//Learned how to use template literals to avoid concatinating a string using the + operator.
