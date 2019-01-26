af330c1094f13baae12c0e6ae85f27af0a8afdcb
Cameron Lucas <44956222+CamSkiTheDev@users.noreply.github.com>
2019-01-26 13:52:36 -0700

    Learned how to structure and else statement to form an if...else statement and preform a take if the condition evaluates to false.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Conditional-Statements/if-else-Statements.js
@@ -0,0 +1,11 @@
+let sale = true;
+
+sale = false;
+
+if (sale) {
+    console.log('Time to buy!');
+} else {
+    console.log('Time to wait for a sale.');
+}
+
+//Learned how to structure and else statement to form an if...else statement and preform a take if the condition evaluates to false.
