26df3f2a13bb23bfde68a4dffde87a39b5b5d407
Cameron Lucas <44956222+CamSkiTheDev@users.noreply.github.com>
2019-01-26 13:35:19 -0700

    Learned how to use the typeof operator to check the data type of a variable.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Variables/typeof-operator.js
@@ -0,0 +1,7 @@
+let newVariable = 'Playing around with typeof.';
+console.log(typeof newVariable);
+
+newVariable = 1;
+console.log(typeof newVariable);
+
+//Learned how to use the typeof operator to check the data type of a variable.
