391d3931a436c5458eb48ac5195da82ddbc57ffc
Cameron Lucas <44956222+CamSkiTheDev@users.noreply.github.com>
2019-01-26 13:57:18 -0700

    Learned how to compare something in javascript using a comparison operator.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Conditional-Statements/Comparison-Operators.js
@@ -0,0 +1,9 @@
+let hungerLevel = 7;
+
+if (hungerLevel > 7) {
+    console.log('Time to eat!');
+} else {
+    console.log('We can eat later!');
+}
+
+//Learned how to compare something in javascript using a comparison operator.
