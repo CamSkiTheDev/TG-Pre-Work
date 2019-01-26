743b6a6413c010a28a86ca3d239f5ccbca662821
Cameron Lucas <44956222+CamSkiTheDev@users.noreply.github.com>
2019-01-26 13:09:25 -0700

    Learned how to create a variable using let in javascript.

--- Code-Academy-Exercises/Introduction-To-JavaScript/Variables/Create-a-Variable.js
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Variables/Create-a-Variable.js
@@ -4,3 +4,10 @@ console.log(favoriteFood);
 console.log(numOfSlices);
 
 //Learned how to create a veriable using var and camel case.
+
+let changeMe = true;
+changeMe = false;
+
+console.log(changeMe);
+
+//Learned how to create a veriable using let and camel case.
