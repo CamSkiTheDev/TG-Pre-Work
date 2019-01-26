a7658693ca462f3764c3c4cb9f2eef0f77bd9591
Cameron Lucas <44956222+CamSkiTheDev@users.noreply.github.com>
2019-01-26 15:25:48 -0700

    Learned how to create a arrow functions.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Functions/Arrow-Functions.js
@@ -0,0 +1,9 @@
+const plantNeedsWater = (day) => {
+    if (day === 'Wednesday') {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+//Learned how to create a arrow functions.  
