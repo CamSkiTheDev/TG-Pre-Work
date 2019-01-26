88fa7d35765129c1ca077a796b2b5e545f0f59e5
Cameron Lucas <44956222+CamSkiTheDev@users.noreply.github.com>
2019-01-26 15:12:12 -0700

    Learned how to create a function that passes back information using the return statement.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Functions/Return.js
@@ -0,0 +1,8 @@
+function monitorCount(rows, columns) {
+    return rows * columns;
+}
+
+const numOfMonitors = monitorCount(5, 4);
+console.log(numOfMonitors);
+
+//Learned how to create a function that passes back information using the return statement.
