964baf89b6b96c2dbabd21b65c35472bb59bc9c9
Cameron Lucas <44956222+CamSkiTheDev@users.noreply.github.com>
2019-01-26 12:27:12 -0700

    Learned about the different types of Arithmetic Operators in js.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Introduction-to-JavaScript/Arithmetic-Operators.js
@@ -0,0 +1,11 @@
+console.log(3.5 + 21);
+
+// Learned the different types of Arithmetic Operators such as....
+
+/*
+    (+) Addition
+    (-) Subtraction
+    (*) multiplication
+    (/) Division
+    (%) Remainder
+*/
