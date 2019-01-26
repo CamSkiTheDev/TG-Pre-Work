c26a4cf78649d988bc4c10af9ad8f2a561e582e1
Cameron Lucas <44956222+CamSkiTheDev@users.noreply.github.com>
2019-01-26 16:05:05 -0700

    Learned that variables that are within a block are only accessable within that block or locally.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Scope/Block-Scope.js
@@ -0,0 +1,9 @@
+const logVisibleLightWaves = () => {
+    const lightWaves = 'Moonlight';
+    console.log(lightWaves);
+}
+
+logVisibleLightWaves();
+console.log(lightWaves);
+
+//Learned that variables that are within a block are only accessable within that block or locally.
