907fe9593917085e3809162a3893e839ec931834
Cameron Lucas <44956222+CamSkiTheDev@users.noreply.github.com>
2019-01-26 14:12:00 -0700

    Learned how to use truthy or falsy and a logical operator to assign values to a variable.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Conditional-Statements/Truthy-and-Falsy-Assignment.js
@@ -0,0 +1,8 @@
+let tool = 'marker';
+
+// Use short circuit evaluation to assign  writingUtensil variable below:
+let writingUtensil = tool || 'pen';
+
+console.log(`The ${writingUtensil} is mightier than the sword.`);
+
+//Learned how to use truthy or falsy and a logical operator to assign values to a variable.
