05ead588628f176b113c7feec0f34f31f2e60b5f
Cameron Lucas <44956222+CamSkiTheDev@users.noreply.github.com>
2019-01-26 14:50:40 -0700

    Learned how to delare a function.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Functions/Function-Declaration.js
@@ -0,0 +1,9 @@
+function getReminder() {
+    console.log('Water the plants.');
+}
+
+function greetInSpanish() {
+    console.log('Buenas Tardes.');
+}
+
+//Learned how to delare a function using the function keyword assigning it a name and a body with the code we want to exacute.
