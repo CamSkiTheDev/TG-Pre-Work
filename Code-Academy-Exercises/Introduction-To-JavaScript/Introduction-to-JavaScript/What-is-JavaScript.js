338ee0f44a1c73dd31a83afc94e18bac76951ef0
Cameron Lucas <44956222+CamSkiTheDev@users.noreply.github.com>
2019-01-26 11:56:25 -0700

    Intro to javascript.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Introduction-to-JavaScript/What-is-JavaScript.js
@@ -0,0 +1,3 @@
+/*  Javascript is a fast and flexable programing language mostly used in the browser. */
+
+/* In this lesson I will learn introductory coding concepts including data types and built-in objects essential knowledge */
