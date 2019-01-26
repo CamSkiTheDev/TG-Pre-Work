8c2e65a1be68659b9015c9176a3fde1f892feeb7
Cameron Lucas <44956222+CamSkiTheDev@users.noreply.github.com>
2019-01-26 12:33:12 -0700

    Got ahead of my self.

--- Code-Academy-Exercises/Introduction-To-JavaScript/Introduction-to-JavaScript/Arithmetic-Operators.js
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Introduction-to-JavaScript/Arithmetic-Operators.js
@@ -1,5 +1,13 @@
 console.log(3.5 + 21);
 
+let year = new Date();
+
+console.log(year.getFullYear() - 1969);
+
+console.log(65 / 240);
+
+console.log(.2708 * 100);
+
 // Learned the different types of Arithmetic Operators such as....
 
 /*
