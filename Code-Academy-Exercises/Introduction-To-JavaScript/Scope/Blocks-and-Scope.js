508684a4b6ca76e56ff9d860f8e86d56cce403d2
Cameron Lucas <44956222+CamSkiTheDev@users.noreply.github.com>
2019-01-26 15:42:33 -0700

    Learned that blocks can access variables outside the block and variables inside the block aswell.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Scope/Blocks-and-Scope.js
@@ -0,0 +1,10 @@
+const city = 'New York City';
+
+const logCitySkyline = () => {
+    let skyscraper = 'Empire State Building';
+    return `The stars over the ${skyscraper} in ${city}`;
+}
+
+console.log(logCitySkyline());
+
+//Learned that blocks can access variables outside the block and variables inside the block aswell.
