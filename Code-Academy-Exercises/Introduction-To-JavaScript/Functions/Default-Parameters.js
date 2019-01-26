9af306dc22e4d0580fdc6c07f41bb1e30e455346
Cameron Lucas <44956222+CamSkiTheDev@users.noreply.github.com>
2019-01-26 15:03:33 -0700

    Learned how to set default values to parameters to be used if no arguments are passed.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Functions/Default-Parameters.js
@@ -0,0 +1,7 @@
+function makeShoppingList(item1 = 'milk', item2 = 'bread', item3 = 'eggs') {
+    console.log(`Remember to buy ${item1}`);
+    console.log(`Remember to buy ${item2}`);
+    console.log(`Remember to buy ${item3}`);
+}
+
+//Learned how to set default values to parameters to be used if no arguments are passed.
