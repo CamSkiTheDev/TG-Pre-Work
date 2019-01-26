b84490b9619a6cac1d2dd0ddb84d8e46add867be
Cameron Lucas <44956222+CamSkiTheDev@users.noreply.github.com>
2019-01-26 14:54:13 -0700

    Learned how to call a function using its name or identifier.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Functions/Calling-a-Function.js
@@ -0,0 +1,9 @@
+function sayThanks() {
+    console.log('Thank you for your purchase! We appreciate your business.');
+}
+
+sayThanks();
+sayThanks();
+sayThanks();
+
+//Learned how to call a function using its name or identifier
