6923d4916f4c86e79d35dbd8bcb3e019d855f04b
Cameron Lucas <44956222+CamSkiTheDev@users.noreply.github.com>
2019-01-26 13:44:06 -0700

    Learned what conditional statements are and how they can help us add logic to our code.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Conditional-Statements/What-are-Conditional-Statements.js
@@ -0,0 +1,10 @@
+//Conditional statements are a way we can make decisions based on conditions
+
+//This will allow us to add logic into our code and carry out code based on the condition of something
+
+if (happy === true) {
+    smile;
+}
+else {
+    beHappy;
+}
