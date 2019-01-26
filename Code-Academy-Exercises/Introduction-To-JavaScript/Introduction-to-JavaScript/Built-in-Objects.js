5bb329a6d47063fd1dcf8b1666f4961568f33cf0
Cameron Lucas <44956222+CamSkiTheDev@users.noreply.github.com>
2019-01-26 12:54:59 -0700

    Learned how to use some of javascripts built in objects.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Introduction-to-JavaScript/Built-in-Objects.js
@@ -0,0 +1,5 @@
+console.log(Math.floor(Math.random() * 100));
+console.log(Math.ceil(43.8));
+console.log(Number.isInteger(2017));
+
+//Learned how to use some of javascript's built in objects to preform tasks like seeing if something is a intieger or genarate a random number
