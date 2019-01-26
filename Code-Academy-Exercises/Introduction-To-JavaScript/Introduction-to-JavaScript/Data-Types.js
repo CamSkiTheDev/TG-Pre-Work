5c1c2f4aa57603e0468218c02422189d934be697
Cameron Lucas <44956222+CamSkiTheDev@users.noreply.github.com>
2019-01-26 12:22:52 -0700

    Learned about the different data types in javascript.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Introduction-to-JavaScript/Data-Types.js
@@ -0,0 +1,16 @@
+console.log('JavaScript');
+console.log(2011);
+console.log('Woohoo! I love to code! #codecademy');
+console.log(20.49)
+
+// Learned about the differnt data types in javascript. 
+
+/*
+    Intigers
+    Strings
+    Booleans
+    Objects
+    Null
+    Undefined
+    Symbols
+*/
