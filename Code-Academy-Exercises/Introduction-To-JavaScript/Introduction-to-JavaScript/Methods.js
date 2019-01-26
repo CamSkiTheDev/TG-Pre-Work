da80b97767c76759bb19c72a2e93ab67a33b6a4e
Cameron Lucas <44956222+CamSkiTheDev@users.noreply.github.com>
2019-01-26 12:46:27 -0700

    Learned how to use methods on strings in javascript.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Introduction-to-JavaScript/Methods.js
@@ -0,0 +1,7 @@
+// Use .toUpperCase() to log 'Codecademy' in all uppercase letters
+console.log('Codecademy'.toUpperCase());
+
+// Use a string method to log the following string without whitespace at the beginning and end of it.
+console.log('    Remove whitespace   '.trim());
+
+//Learned how to use methods to preform task on string like make them upper case and trim the white space around them.
