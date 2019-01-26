7b9c37a2f665aa5e45e153705d8e7033d51432f1
Cameron Lucas <44956222+CamSkiTheDev@users.noreply.github.com>
2019-01-26 14:33:48 -0700

    Learned how to use a switch statement insted of a if...else statement.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Conditional-Statements/The-switch-keyword.js
@@ -0,0 +1,18 @@
+let athleteFinalPosition = 'first place';
+
+switch (athleteFinalPosition) {
+    case 'first place':
+        console.log('You get the gold medal!');
+        break;
+    case 'second place':
+        console.log('You get the silver medal!');
+        break;
+    case 'third place':
+        console.log('You get the bronze medal!');
+        break;
+    default:
+        console.log('No medal awarded.');
+        break;
+}
+
+//Learned how to use a switch statement insted of a if...else statement.
