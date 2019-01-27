e6a02726e9a63db0c8e156311ae158697b68abe6
Cameron Lucas <44956222+CamSkiTheDev@users.noreply.github.com>
2019-01-26 22:29:32 -0700

    Learned how to create a do...while loop.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Loops/Do-While-Statements.js
@@ -0,0 +1,8 @@
+// Write your code below
+let cupsOfSugarNeeded = 9086431;
+let cupsAdded = 0;
+do {
+    cupsAdded++;
+} while (cupsAdded < cupsOfSugarNeeded);
+
+//Learned how to create a do...while loop which will execute code at least once then execute that code while the condition is true
