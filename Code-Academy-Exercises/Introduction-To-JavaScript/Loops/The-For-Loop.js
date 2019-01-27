d93aa65107adb5026910c825a21167cd0d9a2abd
Cameron Lucas <44956222+CamSkiTheDev@users.noreply.github.com>
2019-01-26 21:41:11 -0700

    Learned how to create a for loop.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Loops/The-For-Loop.js
@@ -0,0 +1,7 @@
+// Write your code below
+
+for (let i = 5; i < 11; i++) {
+    console.log(i);
+}
+
+//Learned how to create a for loop and that it contains three expressions (initialization, stopping condition, and iteration statement)
