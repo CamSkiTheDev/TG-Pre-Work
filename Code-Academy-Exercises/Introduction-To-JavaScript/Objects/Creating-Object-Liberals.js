226d3d505c80b4f77bed8a31d5ebd26128181ea5
Cameron Lucas <44956222+CamSkiTheDev@users.noreply.github.com>
2019-01-27 13:38:53 -0700

    Learned how to create a object literal and assign it a few key-value pairs.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Objects/Creating-Object-Liberals.js
@@ -0,0 +1,7 @@
+// Write your fasterShip object literal below
+let fasterShip = {
+    color: 'silver',
+    'Fuel Type': 'Turbo Fuel'
+};
+
+//Learned how to create a object literal and assign it a few key-value pairs
