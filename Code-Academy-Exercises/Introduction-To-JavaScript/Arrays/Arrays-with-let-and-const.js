990098225a616c936bb14abb19ee0b4e8b5727b5
Cameron Lucas <44956222+CamSkiTheDev@users.noreply.github.com>
2019-01-26 20:02:56 -0700

    Learned that we can update items in an array but not re-assign an entire new array when using the const keyword to declare the array.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Arrays/Arrays-with-let-and-const.js
@@ -0,0 +1,14 @@
+let condiments = ['Ketchup', 'Mustard', 'Soy Sauce', 'Sriracha'];
+
+const utensils = ['Fork', 'Knife', 'Chopsticks', 'Spork'];
+
+condiments[0] = 'Mayo';
+console.log(condiments);
+
+condiments = ['Mayo'];
+console.log(condiments);
+
+utensils[3] = 'Spoon';
+console.log(utensils);
+
+//Learned that we can update items in an array but not re-assign an entire new array when using the const keyword to declare the array.
