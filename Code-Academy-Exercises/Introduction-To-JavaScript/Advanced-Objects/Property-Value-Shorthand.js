0823d6c49b23bd3de134a65b295c21916a3894af
Cameron Lucas <44956222+CamSkiTheDev@users.noreply.github.com>
2019-01-27 15:21:45 -0700

    Learned that we can cause unwanted side-effects when mutating a property in a object.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Advanced-Objects/Property-Value-Shorthand.js
@@ -0,0 +1,12 @@
+const robot = {
+    _energyLevel: 100,
+    recharge() {
+        this._energyLevel += 30;
+        console.log(`Recharged! Energy is currently at ${this._energyLevel}%.`)
+    }
+};
+
+robot._energyLevel = 'high';
+robot.recharge();
+
+//Learned that we can cause unwanted side-effects when mutating a property in a object.
