6d1e45368d32a658e4cb061fab65a0a9be7ba6a9
Cameron Lucas <44956222+CamSkiTheDev@users.noreply.github.com>
2019-01-27 16:15:30 -0700

    Learned how to use destructuring to create variables.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Advanced-Objects/Destructured-Assignment.js
@@ -0,0 +1,17 @@
+const robot = {
+    model: '1E78V2',
+    energyLevel: 100,
+    functionality: {
+        beep() {
+            console.log('Beep Boop');
+        },
+        fireLaser() {
+            console.log('Pew Pew');
+        },
+    }
+};
+
+const { functionality } = robot;
+functionality.beep();
+
+//Learned how to use destructuring to create variables in shorthand and save some keystrokes.
