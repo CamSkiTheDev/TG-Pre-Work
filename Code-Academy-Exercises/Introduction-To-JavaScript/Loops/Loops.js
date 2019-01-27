15b64f401056a4e3a4ca0c800cd13317a7cedf0b
Cameron Lucas <44956222+CamSkiTheDev@users.noreply.github.com>
2019-01-26 21:29:39 -0700

    Learned that loops can be used to run code over and over again.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Loops/Loops.js
@@ -0,0 +1,3 @@
+//Learned that loops are used to repeat code over and over until a condition is met. This condition is called a stop condition.
+
+//Once the condition is met the program will move on.
