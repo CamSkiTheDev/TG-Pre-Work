abb1c3d61aa8e594d518dcefbebb7d769f4068ac
Cameron Lucas <44956222+CamSkiTheDev@users.noreply.github.com>
2019-01-26 21:20:56 -0700

    Learned how to create a nested array and how to access an element in a nested array

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Arrays/Nested-Arrays.js
@@ -0,0 +1,4 @@
+const numberClusters = [[1, 2], [3, 4], [5, 6]];
+const target = numberClusters[2][1];
+
+//Learned how to create a nested array and how to access an element in a nested array using bracket notation.
