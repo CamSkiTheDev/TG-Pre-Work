f99a70779ae6860bbec59fddcfb6dc4c0986ad55
Cameron Lucas <44956222+CamSkiTheDev@users.noreply.github.com>
2019-01-27 13:48:04 -0700

    Learned how to access properties of an object using dot notation and its key value.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Objects/Accessing-Properties.js
@@ -0,0 +1,13 @@
+let spaceship = {
+    homePlanet: 'Earth',
+    color: 'silver',
+    'Fuel Type': 'Turbo Fuel',
+    numCrew: 5,
+    flightPath: ['Venus', 'Mars', 'Saturn']
+};
+
+// Write your code below
+const crewCount = spaceship.numCrew;
+const planetArray = spaceship.flightPath;
+
+//Learned how to access properties of an object using dot notation and its key value pair.
