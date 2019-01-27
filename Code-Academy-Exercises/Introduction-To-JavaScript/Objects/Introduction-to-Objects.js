a4b8adec164e22784c6c61ebf733ad92ff83c29c
Cameron Lucas <44956222+CamSkiTheDev@users.noreply.github.com>
2019-01-27 13:32:36 -0700

    Learned what javascript objects are.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Objects/Introduction-to-Objects.js
@@ -0,0 +1,3 @@
+//Learned that javascript objects a containerd that can store information and functions these functions are called methods.
+
+//Learned that we have been using javascript objects this how time and didn't even know it.
