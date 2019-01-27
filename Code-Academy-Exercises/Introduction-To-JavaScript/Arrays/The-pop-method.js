eb2a49aff15eecc151275dec2452466617fa52bd
Cameron Lucas <44956222+CamSkiTheDev@users.noreply.github.com>
2019-01-26 20:46:33 -0700

    Learned how to remove items from the end of an array using the pop method.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Arrays/The-pop-method.js
@@ -0,0 +1,6 @@
+const chores = ['wash dishes', 'do laundry', 'take out trash', 'cook dinner', 'mop floor'];
+
+chores.pop();
+console.log(chores);
+
+//Learned how to remove items from the end of an array using the pop method.
