28d2d178bc9ac9a356bf10a9cae105e59a52b13d
Cameron Lucas <44956222+CamSkiTheDev@users.noreply.github.com>
2019-01-26 19:38:49 -0700

    Learned how to use bracket notation to select an item in an array by its index.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Arrays/Accessing-Elements.js
@@ -0,0 +1,8 @@
+const famousSayings = ['Fortune favors the brave.', 'A joke is a very serious thing.', 'Where there is love there is life.'];
+
+const listItem = famousSayings[0];
+console.log(listItem);
+console.log(famousSayings[2]);
+console.log(famousSayings[3]);
+
+//Learned how to use bracket notation to select an item in an array by its index.
