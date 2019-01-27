a81f28c1a2925766e3bfb1ee362dd2b0b29f5225
Cameron Lucas <44956222+CamSkiTheDev@users.noreply.github.com>
2019-01-26 23:40:29 -0700

    Learned how to use the map method to loop over an array and create a new array.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Iterators/The-map-Method.js
@@ -0,0 +1,17 @@
+const animals = ['Hen', 'elephant', 'llama', 'leopard', 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
+
+// Create the secretMessage array below
+const secretMessage = animals.map(firstLetter => {
+    return firstLetter[0];
+});
+
+console.log(secretMessage.join(''));
+
+const bigNumbers = [100, 200, 300, 400, 500];
+
+// Create the smallNumbers array below
+const smallNumbers = bigNumbers.map(num => {
+    return num / 100;
+});
+
+//Learned how to use the map method to loop over an array and create a new array.
