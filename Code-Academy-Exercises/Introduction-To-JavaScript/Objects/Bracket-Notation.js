2f9eb8d22ad05e4bac6ce5d8d96a0e9369f7811f
Cameron Lucas <44956222+CamSkiTheDev@users.noreply.github.com>
2019-01-27 13:56:01 -0700

    Learned how to use bracket notation to access properties of an object.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Objects/Bracket-Notation.js
@@ -0,0 +1,16 @@
+let spaceship = {
+    'Fuel Type': 'Turbo Fuel',
+    'Active Mission': true,
+    homePlanet: 'Earth',
+    numCrew: 5
+};
+
+let propName = 'Active Mission';
+
+// Write your code below
+const isActive = spaceship['Active Mission'];
+console.log(isActive);
+
+//Learned how to use bracket notation to access properties of an object.
+
+//Learned that we have to use bracket notation if our key includes spaces or special characters
