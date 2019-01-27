fc7e1c19f4355763e63eb7e7f98152d149dac122
Cameron Lucas <44956222+CamSkiTheDev@users.noreply.github.com>
2019-01-27 14:53:32 -0700

    Going to learn advanced object concepts.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Advanced-Objects/Advanced-Objects-Introduction.js
@@ -0,0 +1,40 @@
+const robot = {
+    model: 'B-4MI',
+    mobile: true,
+    greetMaster() {
+        console.log(`I'm model ${this.model}, how may I be of service?`);
+    }
+}
+
+const massProdRobot = (model, mobile) => {
+    return {
+        model,
+        mobile,
+        greetMaster() {
+            console.log(`I'm model ${this.model}, how may I be of service?`);
+        }
+    }
+}
+
+const shinyNewRobot = massProdRobot('TrayHax', true)
+
+const chargingStation = {
+    _name: 'Electrons-R-Us',
+    _robotCapacity: 120,
+    _active: true,
+    _chargingRooms: ['Low N Slow', 'Middle of the Road', 'In and Output'],
+
+    set robotCapacity(newCapacity) {
+        if (typeof newCapacity === 'number') {
+            this._robotCapacity = newCapacity;
+        } else {
+            console.log(`Change ${newCapacity} to a number.`)
+        }
+    },
+    get robotCapacity() {
+        return this._robotCapacity;
+    }
+}
+
+
+//Going to Learn how to use the this keyword, conveying privacy in JavaScript methods, defining getters and setters in objects, creating factory functions, and using destructuring techniques.
