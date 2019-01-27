20f271eb5c3d85853065807fcc5566ad37f12a32
Cameron Lucas <44956222+CamSkiTheDev@users.noreply.github.com>
2019-01-26 19:17:23 -0700

    Learned that arrays are a way to store data they are like list and can store any type of data.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Arrays/Arrays.js
@@ -0,0 +1,5 @@
+let newYearsResolutions = ['Keep a journal', 'Take a falconry class', 'Learn to juggle'];
+
+console.log(newYearsResolutions);
+
+//Learned that arrays are a way to store data they are like list and can store any type of data.
