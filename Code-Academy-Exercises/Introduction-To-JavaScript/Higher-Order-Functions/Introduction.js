18e72c1fe1bc6a5ecfa42bfa3b0f844d31ba8559
Cameron Lucas <44956222+CamSkiTheDev@users.noreply.github.com>
2019-01-26 23:03:05 -0700

    Learned what higher-order-functions are.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Higher-Order-Functions/Introduction.js
@@ -0,0 +1,3 @@
+//Learned that Higher-order functions are functions that accept other functions as arguments or return a function
+
+//This allows us to write more modular code which is easier to read and debug
