04ae707c97dc7ceedf5585c4a76e3627d78f9245
Cameron Lucas <44956222+CamSkiTheDev@users.noreply.github.com>
2019-01-26 21:36:53 -0700

    Learned that repeating a task manually would be vary tedious and this is where a loop can help.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Loops/Repeating-Tasks-Manually.js
@@ -0,0 +1,8 @@
+// Write your code below
+const vacationSpots = ['Colorado', 'Oregon', 'California'];
+
+console.log(vacationSpots[0]);
+console.log(vacationSpots[1]);
+console.log(vacationSpots[2]);
+
+//Learned that repeating a task manually would be vary tedious and this is where a loop can help
