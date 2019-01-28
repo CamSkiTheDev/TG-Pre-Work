81c1b67a62d5ca72465d659a079183ee8fd82d9b
Cameron Lucas <44956222+CamSkiTheDev@users.noreply.github.com>
2019-01-27 20:15:45 -0700

    Learned how to use "caniuse.com" to check the global compatibility of a feature.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Browser-Compatibility-and-Transpilation/caniusedotcom-2.js
@@ -0,0 +1,8 @@
+// Set the variable below to a number
+let esFivePercentageSupport;
+esFivePercentageSupport = 95.38;
+// Set the variable below to a number
+let esSixTemplateLiterals;
+esSixTemplateLiterals = 88.67;
+
+//Learned how to use "caniuse.com" to check the global compatibility of a feature.
