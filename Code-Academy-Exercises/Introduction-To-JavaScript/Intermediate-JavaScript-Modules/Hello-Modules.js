b510d0a265b9145fcec978bc75c9484ea33f0ee3
Cameron Lucas <44956222+CamSkiTheDev@users.noreply.github.com>
2019-01-27 21:26:51 -0700

    Learned that modules make our code easy to fix, debug, and reuse.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Intermediate-JavaScript-Modules/Hello-Modules.js
@@ -0,0 +1,3 @@
+//Learned that Moudules are used to break code into seprate files. Modules can be reused and recycled.
+
+//Find, fix, and debug code more easily and prevent pollution of the global namespace and potential naming collisions
