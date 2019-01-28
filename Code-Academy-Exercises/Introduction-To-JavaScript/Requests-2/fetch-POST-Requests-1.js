bcabcccd4f8805463558a369fc864a1a90c4f757
Cameron Lucas <44956222+CamSkiTheDev@users.noreply.github.com>
2019-01-28 02:17:49 -0700

    Learned how to structure a http post request using the fetch function.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Requests-2/fetch-POST-Requests-1.js
@@ -0,0 +1,2 @@
+//Learned that to send a POST request with the fetch function you have to pass the fetch function two arguments
+//The endpoint and an object that contains information needed for the POST request
