668012eb3c7094097816b68ba5cc31f73870974d
Cameron Lucas <44956222+CamSkiTheDev@users.noreply.github.com>
2019-01-27 20:03:37 -0700

    Learned that not all features of ES6 are supported in every web browser.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Browser-Compatibility-and-Transpilation/Introduction.js
@@ -0,0 +1,10 @@
+var pasta = "Spaghetti"; // ES5 syntax
+
+const meat = "Pancetta"; // ES6 syntax
+
+let sauce = "Eggs and cheese"; // ES6 syntax
+
+// Template literals, like the one below, were introduced in ES6
+const carbonara = `You can make carbonara with ${pasta}, ${meat}, and a sauce made with ${sauce}.`;
+
+//Learned that not all features of ES6 are supported in every web browser and the tools we will use to check compatibility as well of tool use for backwards compatibility.
