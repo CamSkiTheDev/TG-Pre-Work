2c2d2b9d0513925ab55f720604e04650c438128d
Cameron Lucas <44956222+CamSkiTheDev@users.noreply.github.com>
2019-01-27 18:32:10 -0700

    Learned how to create a class and its constructor.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Classes/Constructor.js
@@ -0,0 +1,8 @@
+class Surgeon {
+    constructor(name, department) {
+        this.name = name;
+        this.department = department;
+    }
+}
+
+//Learned how to create a class and its constructor and how the constructor method is called and used.
