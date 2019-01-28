8d05814f9e445c9ca87046e1137eb3f72be29a39
Cameron Lucas <44956222+CamSkiTheDev@users.noreply.github.com>
2019-01-28 02:41:54 -0700

    Learned how to send a HTTP POST request using the fetch function and recive the response.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Requests-2/fetch-POST-Requests-5.js
@@ -0,0 +1,46 @@
+// Information to reach API
+const apiKey = '438cf471af164b5195808f1c7803bf9a';
+const url = 'https://api.rebrandly.com/v1/links';
+
+// Some page elements
+const inputField = document.querySelector('#input');
+const shortenButton = document.querySelector('#shorten');
+const responseField = document.querySelector('#responseField');
+
+// AJAX functions
+const shortenUrl = () => {
+    const urlToShorten = inputField.value;
+    const data = JSON.stringify({ destination: urlToShorten });
+
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json',
+            'apikey': apiKey
+        },
+        body: data
+    }).then(response => {
+        if (response.ok) {
+            return response.json();
+        }
+        throw new Error('Request failed!');
+    }, networkError => {
+        console.log(networkError.message);
+    }).then(jsonResponse => {
+        renderResponse(jsonResponse);
+    });
+}
+
+// Clear page and call AJAX functions
+const displayShortUrl = (event) => {
+    event.preventDefault();
+    while (responseField.firstChild) {
+        responseField.removeChild(responseField.firstChild)
+    }
+    shortenUrl();
+}
+
+shortenButton.addEventListener('click', displayShortUrl);
+
+
+//Learned how to send a HTTP POST request using the fetch function and recive the response.
