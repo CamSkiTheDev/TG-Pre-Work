7caeec4888c83f84a15de0d46d66031909dc3ebd
Cameron Lucas <44956222+CamSkiTheDev@users.noreply.github.com>
2019-01-27 18:37:27 -0700

    Learned how to create a new insatnce of a class using the new keywork and save the insatnce to a variable.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Classes/Instance.js
@@ -0,0 +1,11 @@
+class Surgeon {
+    constructor(name, department) {
+        this.name = name;
+        this.department = department;
+    }
+}
+
+const surgeonCurry = new Surgeon('Curry', 'Cardiovascular');
+const surgeonDurant = new Surgeon('Durant', 'Orthopedics');
+
+//Learned how to create a new insatnce of a class using the new keywork and save the insatnce to a variable.
