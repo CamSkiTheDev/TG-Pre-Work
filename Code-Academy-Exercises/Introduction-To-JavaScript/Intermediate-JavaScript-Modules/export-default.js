e43e954a5fd328824627e0b169d8a6a599ab065d
Cameron Lucas <44956222+CamSkiTheDev@users.noreply.github.com>
2019-01-27 22:05:27 -0700

    Learned that we can use the es6 default export syntax to export modules.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Intermediate-JavaScript-Modules/export-default.js
@@ -0,0 +1,7 @@
+const Airplane = {
+    availableAirplanes: [{ name: 'AeroJet', fuelCapacity: 800 }, { name: 'SkyJet', fuelCapacity: 500 }]
+};
+
+export default Airplane;
+
+//Learned that we can use the es6 default export syntax to export modules which is easier to read and more flexible.
