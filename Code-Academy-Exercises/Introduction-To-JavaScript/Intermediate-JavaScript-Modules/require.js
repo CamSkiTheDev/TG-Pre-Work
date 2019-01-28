3f94d0b40bd5b12059e419e244c7cc499bb8e502
Cameron Lucas <44956222+CamSkiTheDev@users.noreply.github.com>
2019-01-27 21:40:41 -0700

    Learned how to import a module and use it in our program.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Intermediate-JavaScript-Modules/require.js
@@ -0,0 +1,9 @@
+const Airplane = require('./1-airplane.js');
+
+const displayAirplane = () => {
+    console.log(Airplane.myAirplane);
+};
+
+displayAirplane();
+
+//Learned how to import a module and use it in our program using the require function.
